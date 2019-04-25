d30b993ceb55a1f7d4e84d8e23e942a2bb258276
Vallabh Kansagara <vallabh.kansagara@cygnetinfotech.com>
2019-04-25 19:08:14 +0530

    adding mysql connector.

---
+++ server/mysql.c
@@ -0,0 +1,46 @@
+/*
+Simple C program that connects to MySQL Database server
+Compile using  = gcc -o mysql-output $(mysql_config --cflags) mysql.c $(mysql_config --libs)
+Pkg = sudo apt-get install libmysqlclient-dev
+*/
+
+#include <mysql.h>
+#include <stdio.h>
+
+int main() {
+   MYSQL *conn;
+   MYSQL_RES *res;
+   MYSQL_ROW row;
+
+   char *server = "localhost";
+   char *user = "root";
+   char *password = "root"; /* set me first */
+   char *database = "mysql";
+
+   conn = mysql_init(NULL);
+
+   /* Connect to database */
+   if (!mysql_real_connect(conn, server,
+         user, password, database, 0, NULL, 0)) {
+      fprintf(stderr, "%s\n", mysql_error(conn));
+      exit(1);
+   }
+
+   /* send SQL query */
+   if (mysql_query(conn, "show tables")) {
+      fprintf(stderr, "%s\n", mysql_error(conn));
+      exit(1);
+   }
+
+   res = mysql_use_result(conn);
+
+   /* output table name */
+   printf("MySQL Tables in mysql database:\n");
+   while ((row = mysql_fetch_row(res)) != NULL)
+      printf("%s \n", row[0]);
+
+   /* close connection */
+   mysql_free_result(res);
+   mysql_close(conn);
+   return (0);
+}
