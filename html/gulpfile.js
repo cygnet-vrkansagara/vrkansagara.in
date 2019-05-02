cb7f3aab8a3ce643da93f0e4634e996b75ca3da1
Vallabh Kansagara <vrkansagara@gmail.com>
2019-05-03 01:22:34 +0530

    updating images and gulp file
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -2,15 +2,20 @@
 
 var gulp = require('gulp');
 var path = require('path');
-var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
+var sass = require('gulp-sass');
+
+var minifyCSS = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 var Paths = {
     HERE: './',
     DIST: 'dist/',
     CSS: './assets/css/',
+    JS: './assets/js/',
     SCSS_TOOLKIT_SOURCES: './assets/scss/material-kit.scss',
     SCSS: './assets/scss/**/**'
 };
@@ -33,4 +38,33 @@ gulp.task('open', function () {
         .pipe(open());
 });
 
-gulp.task('default', ['open', 'watch']);
+
+//:::ASSETS
+//::CSS
+var assetCssBundle = [
+    'assets/css/font-awesome.css',
+    'assets/css/material-kit.css',
+    'assets/demo/demo.css'
+];
+gulp.task('asset:minify:css', function () {
+    return gulp.src(assetCssBundle)
+        .pipe(concat('app.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(Paths.CSS));
+});
+//::JS
+var commonJSBundle = [
+    'assets/js/core/jquery.min.js',
+    'assets/js/core/popper.min.js',
+    'assets/js/core/bootstrap-material-design.min.js',
+    'assets/js/material-kit.js'
+];
+gulp.task('asset:minify:js', function () {
+    return gulp.src(commonJSBundle)
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(Paths.JS));
+});
+
+
+gulp.task('default', ['open', 'watch', 'asset:minify:css', 'asset:minify:js']);
