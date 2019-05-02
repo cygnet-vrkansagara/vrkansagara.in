a9a2a27949a9ce9ebaa5cf7ef1391cd14f854cd3
Vallabh Kansagara <vrkansagara@gmail.com>
2019-05-03 00:55:39 +0530

    updating images
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var gulp = require('gulp');
 var path = require('path');
 var sass = require('gulp-sass');
@@ -6,29 +8,29 @@ var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
 
 var Paths = {
-  HERE: './',
-  DIST: 'dist/',
-  CSS: './assets/css/',
-  SCSS_TOOLKIT_SOURCES: './assets/scss/material-kit.scss',
-  SCSS: './assets/scss/**/**'
+    HERE: './',
+    DIST: 'dist/',
+    CSS: './assets/css/',
+    SCSS_TOOLKIT_SOURCES: './assets/scss/material-kit.scss',
+    SCSS: './assets/scss/**/**'
 };
 
-gulp.task('compile-scss', function() {
-  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write(Paths.HERE))
-    .pipe(gulp.dest(Paths.CSS));
+gulp.task('compile-scss', function () {
+    return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write(Paths.HERE))
+        .pipe(gulp.dest(Paths.CSS));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(Paths.SCSS, ['compile-scss']);
+gulp.task('watch', function () {
+    gulp.watch(Paths.SCSS, ['compile-scss']);
 });
 
-gulp.task('open', function() {
-  gulp.src('index.html')
-    .pipe(open());
+gulp.task('open', function () {
+    gulp.src('index.html')
+        .pipe(open());
 });
 
-gulp.task('open-app', ['open', 'watch']);
+gulp.task('default', ['open', 'watch']);
