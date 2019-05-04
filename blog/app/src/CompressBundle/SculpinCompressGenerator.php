ddbbd4aac37c163b9c5eaade50fa21a59a6bb923
Vallabh Kansagara <vrkansagara@gmail.com>
2019-05-04 12:38:16 +0530

    Adding html compressor
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

--- blog/app/src/CompressBundle/SculpinCompressGenerator.php
+++ blog/app/src/CompressBundle/SculpinCompressGenerator.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace Vrkansagara\Blog\CompressBundle;
 
+use Sculpin\Core\Event\ConvertEvent;
 use Sculpin\Core\Sculpin;
 use Sculpin\Core\Event\SourceSetEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -10,44 +12,146 @@ class CompressGenerator implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            Sculpin::EVENT_BEFORE_RUN => 'beforeRun',
+//            Sculpin::EVENT_AFTER_FORMAT => 'afterFormat',
         );
     }
 
-    public function beforeRun(SourceSetEvent $sourceSetEvent)
+    public function afterFormat(SourceSetEvent $sourceSetEvent)
     {
-        $sourceSet = $sourceSetEvent->sourceSet();
-
-        foreach ($sourceSet->updatedSources() as $source) {
-            if ($source->isGenerated()) {
-                // Skip generated sources.
-                continue;
-            }
-
-            if (!$source->data()->get('redirect')) {
-                // Skip source that do not have redirect.
-                continue;
-            }
-
-            foreach ($source->data()->get('redirect') as $key => $redirect) {
-                // Clone current search with new sourceId.
-                $generatedSource = $source->duplicate($source->sourceId() . ':' . $redirect);
-
-                // Set destination is original source.
-                $generatedSource->data()->set('destination', $source);
-
-                // Overwrite permalink.
-                $generatedSource->data()->set('permalink', $redirect);
-
-                // Add redirect.
-                $generatedSource->data()->set('layout', 'redirect');
-
-                // Make sure Sculpin knows this source is generated.
-                $generatedSource->setIsGenerated();
-
-                // Add the generated source to the source set.
-                $sourceSet->mergeSource($generatedSource);
-            }
+        foreach ($sourceSetEvent->allSources() as $index => $file) {
+            $file->setFormattedContent($this->compressHtml($file->formattedContent()));
         }
     }
+
+    /**
+     * This method will no longer support.
+     *
+     * @note Code will be healed even after marked as @deprecated for further reference.
+     *
+     * @param $buffer
+     *
+     * @return null|string|string[] Compressed output
+     */
+    public function compressHtml($buffer)
+    {
+
+        $whiteSpaceRules = array(
+            '/(\s)+/s' => '\\1',// shorten multiple whitespace sequences
+            "#>\s+<#" => ">\n<",  // Strip excess whitespace using new line
+            "#\n\s+<#" => "\n<",// strip excess whitespace using new line
+            '/\>[^\S ]+/s' => '>',
+            // Strip all whitespaces after tags, except space
+            '/[^\S ]+\</s' => '<',// strip whitespaces before tags, except space
+            /**
+             * '/\s+     # Match one or more whitespace characters
+             * (?!       # but only if it is impossible to match...
+             * [^<>]*   # any characters except angle brackets
+             * >        # followed by a closing bracket.
+             * )         # End of lookahead
+             * /x',
+             */
+            //            '/\s+(?![^<>]*>)/x' => '', //Remove all whitespaces except content between html tags. //MOST DANGEROUS
+        );
+        $commentRules = array(
+            "/<!--.*?-->/ms" => '',// Remove all html comment.,
+        );
+        $replaceWords = array(
+            //OldWord will be replaced by the NewWord
+            //              '/\bOldWord\b/i' =>'NewWord' // OldWord <-> NewWord DO NOT REMOVE THIS LINE. {REFERENCE LINE}
+        );
+        $allRules = array_merge(
+            $replaceWords,
+            $commentRules,
+            $whiteSpaceRules
+        );
+        $buffer = $this->compressJscript($buffer);
+        $buffer = preg_replace(
+            array_keys($allRules), array_values($allRules), $buffer
+        );
+
+
+        /**
+         * To remove useless whitespace from generated HTML, except for Javascript.
+         * [Regex Source]
+         * https://github.com/bcit-ci/codeigniter/wiki/compress-html-output
+         * http://stackoverflow.com/questions/5312349/minifying-final-html-output-using-regular-expressions-with-codeigniter
+         * %# Collapse ws everywhere but in blacklisted elements.
+         * (?>             # Match all whitespaces other than single space.
+         * [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+         * | \s{2,}        # or two or more consecutive-any-whitespace.
+         * ) # Note: The remaining regex consumes no text at all...
+         * (?=             # Ensure we are not in a blacklist tag.
+         * (?:           # Begin (unnecessary) group.
+         * (?:         # Zero or more of...
+         * [^<]++    # Either one or more non-"<"
+         * | <         # or a < starting a non-blacklist tag.
+         * (?!/?(?:textarea|pre)\b)
+         * )*+         # (This could be "unroll-the-loop"ified.)
+         * )             # End (unnecessary) group.
+         * (?:           # Begin alternation group.
+         * <           # Either a blacklist start tag.
+         * (?>textarea|pre)\b
+         * | \z          # or end of file.
+         * )             # End alternation group.
+         * )  # If we made it here, we are not in a blacklist tag.
+         * %ix
+         */
+        $regexRemoveWhiteSpace
+            = '%(?>[^\S ]\s*| \s{2,})(?=(?:(?:[^<]++| <(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))%ix';
+        $new_buffer = preg_replace($regexRemoveWhiteSpace, '', $buffer);
+        // We are going to check if processing has working
+        if ($new_buffer === null) {
+            $new_buffer = $buffer;
+        }
+        return $new_buffer;
+    }
+
+    public function compressJscript($buffer)
+    {
+        // JavaScript compressor by John Elliot <jj5@jj5.net>
+        $replace = array(
+            '#\'([^\n\']*?)/\*([^\n\']*)\'#' => "'\1/'+\'\'+'*\2'",
+            // remove comments from ' strings
+            '#\"([^\n\"]*?)/\*([^\n\"]*)\"#' => '"\1/"+\'\'+"*\2"',
+            // remove comments from " strings
+            '#/\*.*?\*/#s' => "",// strip C style comments
+            '#[\r\n]+#' => "\n",
+            // remove blank lines and \r's
+            '#\n([ \t]*//.*?\n)*#s' => "\n",
+            // strip line comments (whole line only)
+            '#([^\\])//([^\'"\n]*)\n#s' => "\\1\n",
+            // strip line comments
+            // (that aren't possibly in strings or regex's)
+            '#\n\s+#' => "\n",// strip excess whitespace
+            '#\s+\n#' => "\n",// strip excess whitespace
+            '#(//[^\n]*\n)#s' => "\\1\n",
+            // extra line feed after any comments left
+            // (important given later replacements)
+            '#/([\'"])\+\'\'\+([\'"])\*#' => "/*"
+            // restore comments in strings
+        );
+        $script = preg_replace(array_keys($replace), $replace, $buffer);
+        $replace = array(
+            "&&\n" => "&&",
+            "||\n" => "||",
+            "(\n" => "(",
+            ")\n" => ")",
+            "[\n" => "[",
+            "]\n" => "]",
+            "+\n" => "+",
+            ",\n" => ",",
+            "?\n" => "?",
+            ":\n" => ":",
+            ";\n" => ";",
+            "{\n" => "{",
+            //  "}\n"  => "}", (because I forget to put semicolons after function assignments)
+            "\n]" => "]",
+            "\n)" => ")",
+            "\n}" => "}",
+            "\n\n" => "\n",
+        );
+        $script = str_replace(array_keys($replace), $replace, $script);
+        return trim($script);
+    }
+
 }
